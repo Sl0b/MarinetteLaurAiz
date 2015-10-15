b61869689a7b6576d4289d9903b2448715f88003
Arano <etsaiak2@hotmail.fr>
2015-10-15 23:11:11 +0200

    bideoak

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,13 +1,20 @@
 // MAIN CONTROLLER
 function mainController($scope, listVilleService) {
-  $scope.title = "Marinette";
+  $scope.title = "Itsaso bazterrak";
   $scope.villes = [
-    {nom: "Biarritz", video: "https://www.youtube.com/embed/SblTNCEnhIw"},
-    {nom: "Donostia", video: "https://player.vimeo.com/video/142316610"},
-    {nom: "Lekeitio", video: "https://www.youtube.com/embed/SblTNCEnhIw"},
-    {nom: "Orio", video: "https://www.youtube.com/embed/SblTNCEnhIw"},
-    {nom: "Mundaka", video: "https://www.youtube.com/embed/SblTNCEnhIw"},
-    {nom: "Bidarte", video: "https://www.youtube.com/embed/SblTNCEnhIw"}
+    {nom: "Biarritze", video: "https://www.youtube.com/embed/ru7SHhfRftU?autoplay=1"},
+    {nom: "Donostia", video: "https://www.youtube.com/embed/k341rzauQpg?autoplay=1"},
+    {nom: "Lekeitio", video: "https://www.youtube.com/embed/t3Ra_GJUzBg?autoplay=1"},
+    {nom: "Orio", video: "https://www.youtube.com/embed/SblTNCEnhIw?autoplay=1"},
+    {nom: "Mundaka", video: "https://www.youtube.com/embed/l0RTQl4dP00?autoplay=1"},
+    {nom: "Bakio", video: "https://www.youtube.com/embed/7bSgKMIBSk8?autoplay=1"},
+    {nom: "Zarautz", video: "https://www.youtube.com/embed/D6loP8PXAOk?autoplay=1"},
+    {nom: "Bermeo", video: "https://www.youtube.com/embed/W7DCtgBgmjw?autoplay=1"},
+    {nom: "Zumaia", video: "https://www.youtube.com/embed/Ob6vjOFnVfo?autoplay=1"},
+    {nom: "Getaria", video: "https://www.youtube.com/embed/HDL7qxG84Wk?autoplay=1"},
+    {nom: "Ondarroa", video: "https://www.youtube.com/embed/UG73XWkFx3A?autoplay=1"},
+    {nom: "Hondarribia", video: "https://www.youtube.com/embed/xVt4T2VvH_g?autoplay=1"}
+
   ];
   var data = {};
   var image = document.querySelector('.image')
@@ -23,7 +30,7 @@ function mainController($scope, listVilleService) {
     var marker = new google.maps.Marker({
       position: {lat: data.coord.lat, lng: data.coord.lon},
       map: myMap,
-      title: 'Hello World!'
+      title: 'Aurkitu duzu!'
     });
   }
 
