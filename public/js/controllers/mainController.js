cec3fc8c5a08aec71d8a6b5371ed7a300bf21966
Laurent Berthier <proberthier@gmail.com>
2015-10-15 18:09:21 +0200

    new stuff

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,8 +1,17 @@
 // MAIN CONTROLLER
 function mainController($scope, listVilleService) {
   $scope.title = "Marinette";
+  $scope.villes = [
+    {nom: "Biarritz", video: "https://www.youtube.com/embed/SblTNCEnhIw"},
+    {nom: "Donostia", video: "https://www.youtube.com/embed/SblTNCEnhIw"},
+    {nom: "Lekeitio", video: "https://www.youtube.com/embed/SblTNCEnhIw"},
+    {nom: "Orio", video: "https://www.youtube.com/embed/SblTNCEnhIw"},
+    {nom: "Mundaka", video: "https://www.youtube.com/embed/SblTNCEnhIw"},
+    {nom: "Bidarte", video: "https://www.youtube.com/embed/SblTNCEnhIw"}
+  ];
   var data = {};
   var image = document.querySelector('.image')
+  var video = document.querySelector('.video')
 
   function initialize() {
     var mapProp = {
@@ -19,11 +28,23 @@ function mainController($scope, listVilleService) {
   }
 
   $scope.change = function(){
-    listVilleService.getWeather($scope.ville).then(function(response){
+    $scope.villes.forEach(function(ville){
+      if (ville.nom == $scope.selectedNameVille){
+        $scope.selectedVille = ville;
+        getInfos(ville);
+        return;
+      }
+    });
+
+  }
+  
+  function getInfos(ville){
+   listVilleService.getWeather(ville.nom).then(function(response){
       data = response.data;
       $scope.temp = Math.round(data.main.temp);
       $scope.meteo = data.weather[0].main;
       image.src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+      video.src =  ville.video;
       initialize();
       $scope.myValue = true;
       console.log(response);
